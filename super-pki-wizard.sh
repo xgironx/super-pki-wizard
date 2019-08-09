7a9e9c56f6465dedab8c5d734dffcd972e46cfdc
xgironx <xgironx@gmail.com>
2019-08-09 15:27:33 -0400

    switched to array file for input, deleted old output dirs

--- super-pki-wizard.sh
+++ super-pki-wizard.sh
@@ -6,7 +6,7 @@ export              TOP_PID=$$
 
 function    fn_exit_with_error 
 {
-                    message=${1}
+                   message=${1}
     >&2     echo ${message}
     kill -s TERM ${TOP_PID}
 }
@@ -48,15 +48,13 @@ function    fn_init_defaults
 
 function    fn_lowercase_cn
 {
-
            local  COMMON_NAME=${1}
                   COMMON_NAME_LC=`echo ${COMMON_NAME}    | tr    '[:upper:]' '[:lower:]'`
            echo ${COMMON_NAME_LC}
 }
 
 function    fn_set_filename_vars 
-{
-        
+{     
     ENV_SCRIPT='super-pki-wizard-env.sh'
         KEY_PRIVATE_ENCRYPTED=${COMMON_NAME_LC}.key-private-encrypted.pem
     KEY_PRIVATE_ENCRYPTED_NOT=${COMMON_NAME_LC}.key-private-encrypted-not.pem
@@ -88,71 +86,21 @@ function    fn_read_optional
                VAR_NAME=${1}
     DEFAULT=${!VAR_NAME}
               PROMPT=${2}
-    read -p ${PROMPT}:    REPLY
+    read -p ${PROMPT}:                    REPLY
                 if                 [ -n ${REPLY} ]; then
                     eval "${VAR_NAME}=\"${REPLY}\""
                 fi
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function    fn_gen_key_private
 {
-    #echo "_______________________"
     echo "  --inside fn_gen_key_private"
     echo   `pwd`                            '  --PWD'
-    echo   ${DIR_OUT}                       '  --DIR_OUT'
-    #openssl genrsa  -out        server201907221709/${KEY_PRIVATE_ENCRYPTED_NOT}  2048                                          
-    #openssl genrsa  -out        $DIR_OUT/${KEY_PRIVATE_ENCRYPTED_NOT}  2048                                                    
+    echo   ${DIR_OUT}                       '  --DIR_OUT'                                                   
     openssl genrsa  -out        ${DIR_OUT}/${KEY_PRIVATE_ENCRYPTED_NOT}  2048                                                   
     openssl rsa     -des3   -in ${DIR_OUT}/${KEY_PRIVATE_ENCRYPTED_NOT}        -out ${DIR_OUT}/${KEY_PRIVATE_ENCRYPTED}    -passout "file:${DIR_OUT}/${FILE_PASSWORD}"   #ENCIPHER          PRIVATE KEY
-    chmod 0600 ${DIR_OUT}/${KEY_PRIVATE_ENCRYPTED_NOT}
-    chmod 0600 ${DIR_OUT}/${KEY_PRIVATE_ENCRYPTED}
-   #openssl rsa     -des3   -in ${KEY_PRIVATE_ENCRYPTED_NOT}        -out ${KEY_PRIVATE_ENCRYPTED}    -passout "file:${DIR_OUT}/${FILE_PASSWORD}"   #ENCIPHER          PRIVATE KEY
+                     chmod 0600 ${DIR_OUT}/${KEY_PRIVATE_ENCRYPTED_NOT}
+                     chmod 0600 ${DIR_OUT}/${KEY_PRIVATE_ENCRYPTED}
 }				
 
 
@@ -168,15 +116,14 @@ function    fn_mkdir_out
 {
                                                                                    echo ${COMMON_NAME}
                                                      FILENAME_PREFIX=$(fn_lowercase_cn  ${COMMON_NAME})
-                                              echo ${FILENAME_PREFIX}
-                                         
-                                          DIR_OUT="${FILENAME_PREFIX}-$((`date '+%Y'` + 3))"                        
+                                              echo ${FILENAME_PREFIX}                                         
+                                          #DIR_OUT="${FILENAME_PREFIX}-$((`date '+%Y'` + 3))"                        
+                                          DIR_OUT=${FILENAME_PREFIX}-$((`date '+%Y'` + 3))                      
     if  [[ -d                           ${DIR_OUT}      ]]; then
                     i=1
         while   [[ -d                   ${DIR_OUT}.$i   ]]; do
                 let i++
-        done
-                      
+        done                  
     fi
                                  mkdir  ${DIR_OUT}
                             chmod 0770  ${DIR_OUT}
@@ -186,15 +133,9 @@ function    fn_mkdir_out
 function    fn_gen_enviro_script
 {
     cat  << EOF > ${ENV_SCRIPT}
-#!/bin/bash
-             
-             
-             
+#!/bin/bash          
 export SSP=${SSP}
-DIR_BASE='/etc/pki'
-             
-                        
-             
+DIR_BASE='/etc/pki'           
 DIR_PKI='\${DIR_BASE}/\${COMMON_NAME}'
 if  [ -r '\${DIR_PKI/password.txt' ]; then
     PKI_PASSWORD=\$(  cat '\${DIR_PKI}/password.txt'  )
@@ -210,41 +151,22 @@ function fn_record_password {
 
 
 function    fn_gen_key_and_csr
-{
-                                                            
-    IFS=,
-                                                            
+{                                                         
+    IFS=,                                                      
     fn_init_defaults
     mkdir       requests && \
     chmod 0770  requests
-    while read -a COMMON_NAME; do
-                                                 
-                                                 
-            for x in ${COMMON_NAME[@]}; do echo ${x}; done;
-                                                 
-                                                 
-                                                 
+    while read -a COMMON_NAME; do                                                                                            
+            for x in                 ${COMMON_NAME[@]}; do echo ${x}; done;                                 
             fn_set_filename_vars     ${COMMON_NAME}
             fn_mkdir_out
-                                                 
             fn_record_password
-                                                 
-                                                 
             echo `pwd`                                      '  --PWD'
-                                                 
             echo ${DIR_OUT}                                 '  --DIR_OUT'
-                                                 
-                                                 
-                                                            
-                                                 
             echo "NOW:  fn_gen_key_private"
                         fn_gen_key_private
-            fn_gen_key_public_ssh
-            cd -
-                                                 
-                                                 
-                                         
-                                         
+                        fn_gen_key_public_ssh
+#           cd -                               
         done    < hostinfo.txt.array
 }
 
